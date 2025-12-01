99733fedb90bc772f710059a3f9d824580ae4891
Asad <maza21asad@gmail.com>
2025-12-01 17:30:07 +0600

    Rabbit Animation functionality Added

--- Assets/Scripts/PillFill Scripts/PillFill_LevelManager.cs
+++ Assets/Scripts/PillFill Scripts/PillFill_LevelManager.cs
@@ -1,10 +1,99 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+
+//public class PillFill_LevelManager : MonoBehaviour
+//{
+//    public static PillFill_LevelManager Instance;
+
+//    public PillFill_LevelData[] levels;
+
+//    private int currentLevelIndex = 0;
+
+//    private int collectedPills = 0;
+//    private int requiredPills = 0;
+
+//    private void Awake()
+//    {
+//        Instance = this;
+//    }
+
+//    private void Start()
+//    {
+//        LoadLevel(0); // Start Level 1
+//    }
+
+//    public void LoadLevel(int index)
+//    {
+//        currentLevelIndex = index;
+
+//        PillFill_LevelData data = levels[index];
+
+//        collectedPills = 0;
+//        requiredPills = data.requiredPillCount;
+
+//        // spawn pills + obstacles
+//        PillFill_ItemSpawner.Instance.SpawnLevel(data);
+
+//        //Debug.Log("Loaded Level: " + data.levelName);
+//    }
+
+//    public void RegisterPillCollected()
+//    {
+//        collectedPills++;
+
+//        if (collectedPills >= requiredPills)
+//        {
+//            OnLevelCompleted();
+//        }
+//    }
+
+//    private void OnLevelCompleted()
+//    {
+//        Debug.Log("LEVEL COMPLETED: " + (currentLevelIndex + 1));
+
+//        //LoadNextLevel();
+//        PillFill_LevelUI.Instance.ShowLevelComplete(currentLevelIndex + 1);
+
+//        Invoke(nameof(LoadNextLevel), 3.5f); // wait 2 sec before next level
+//    }
+
+//    public void LoadNextLevel()
+//    {
+//        int nextIndex = currentLevelIndex + 1;
+
+//        if (nextIndex >= levels.Length)
+//        {
+//            Debug.Log("All 5 levels completed!");
+//            return;
+//        }
+
+//        // HIDE the previous level popup
+//        //PillFill_LevelUI.Instance.HideLevelComplete();
+
+//        LoadLevel(nextIndex);
+//    }
+//}
+
+
+using UnityEngine;
+using System.Collections;
 
 public class PillFill_LevelManager : MonoBehaviour
 {
     public static PillFill_LevelManager Instance;
 
+    [Header("Level Data")]
     public PillFill_LevelData[] levels;
+
+    [Header("Rabbit Animations")]
+    public Animator rabbitAnimator;
+
+    // Idle animation per level (size = number of levels)
+    public string[] rabbit_idleAnimations;
+
+    // Revive animation between levels (size = number of levels - 1)
+    public string[] rabbit_reviveAnimations;
+
+    public string rabbit_happyAnimation;
 
     private int currentLevelIndex = 0;
 
@@ -18,7 +107,7 @@ public class PillFill_LevelManager : MonoBehaviour
 
     private void Start()
     {
-        LoadLevel(0); // Start Level 1
+        LoadLevel(0);
     }
 
     public void LoadLevel(int index)
@@ -30,10 +119,20 @@ public class PillFill_LevelManager : MonoBehaviour
         collectedPills = 0;
         requiredPills = data.requiredPillCount;
 
-        // spawn pills + obstacles
+        // Spawn items
         PillFill_ItemSpawner.Instance.SpawnLevel(data);
 
-        //Debug.Log("Loaded Level: " + data.levelName);
+        // Play this level's rabbit idle animation
+        PlayRabbitIdle(currentLevelIndex);
+    }
+
+    private void PlayRabbitIdle(int levelIndex)
+    {
+        if (levelIndex < rabbit_idleAnimations.Length)
+        {
+            string anim = rabbit_idleAnimations[levelIndex];
+            rabbitAnimator.Play(anim);
+        }
     }
 
     public void RegisterPillCollected()
@@ -42,18 +141,45 @@ public class PillFill_LevelManager : MonoBehaviour
 
         if (collectedPills >= requiredPills)
         {
-            OnLevelCompleted();
+            StartCoroutine(HandleLevelComplete());
         }
     }
 
-    private void OnLevelCompleted()
+    private IEnumerator HandleLevelComplete()
     {
-        Debug.Log("LEVEL COMPLETED: " + (currentLevelIndex + 1));
+        // Step 1: Play revive animation (except after last level)
+        if (currentLevelIndex < rabbit_reviveAnimations.Length)
+        {
 
-        //LoadNextLevel();
+            yield return new WaitForSeconds(1f);
+            string reviveAnim = rabbit_reviveAnimations[currentLevelIndex];
+            rabbitAnimator.Play(reviveAnim);
+
+            //// Wait revive animation time
+            //yield return new WaitForSeconds(
+            //    rabbitAnimator.GetCurrentAnimatorStateInfo(0).length
+            //);
+            ////rabbitAnimator.Play(reviveAnim);
+            ///
+
+            // Wait 1 frame so Animator updates to reviveAnim
+            yield return null;
+
+            // Read correct revive animation length
+            float reviveLength = rabbitAnimator.GetCurrentAnimatorStateInfo(0).length;
+
+            // Play revive animation fully (no loop)
+            yield return new WaitForSeconds(reviveLength);
+        }
+
+        // Step 2: Show level complete popup (UI handles the animation)
         PillFill_LevelUI.Instance.ShowLevelComplete(currentLevelIndex + 1);
 
-        Invoke(nameof(LoadNextLevel), 3.5f); // wait 2 sec before next level
+        // Step 3: Wait until popup animation ends
+        yield return new WaitForSeconds(2.2f);
+
+        // Step 4: Load next level
+        //LoadNextLevel();
     }
 
     public void LoadNextLevel()
@@ -62,12 +188,12 @@ public class PillFill_LevelManager : MonoBehaviour
 
         if (nextIndex >= levels.Length)
         {
-            Debug.Log("All 5 levels completed!");
+            Debug.Log("All levels finished!");
+
+            rabbitAnimator.Play(rabbit_happyAnimation);
+
             return;
         }
-
-        // HIDE the previous level popup
-        //PillFill_LevelUI.Instance.HideLevelComplete();
 
         LoadLevel(nextIndex);
     }
