0b2bbd3f2b052b5dc6a315d67db8d67750cde332
Asad <maza21asad@gmail.com>
2026-03-09 11:02:04 +0600

    Ele animation fix somehow

--- Assets/Scripts/Mix-A-Fix Scripts/Mix_A_Fix/MixAFix_DollController.cs
+++ Assets/Scripts/Mix-A-Fix Scripts/Mix_A_Fix/MixAFix_DollController.cs
@@ -1,10 +1,68 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+
+//public class MixAFix_DollController : MonoBehaviour
+//{
+//    public static MixAFix_DollController Instance;
+
+//    private Animator animator;
+
+//    private void Awake()
+//    {
+//        Instance = this;
+//        animator = GetComponent<Animator>();
+//    }
+
+//    // Triggered when a correct ingredient is accepted
+//    public void PlaySuccess()
+//    {
+//        //if (animator != null) animator.Play("Success", -1, 0f);
+//        if (animator != null) animator.Play("Ele_Yes", -1, 0f);
+//        //Debug.Log("Success");
+//        Debug.Log("Ele_Yes");
+//    }
+
+//    // Triggered when a wrong ingredient is dropped
+//    public void PlaySad()
+//    {
+//        //if (animator != null) animator.Play("Sad", -1, 0f);
+//        if (animator != null) animator.Play("Ele_NO", -1, 0f);
+//        //Debug.Log("Sad");
+//        Debug.Log("Ele_NO");
+//    }
+
+//    // Triggered when the entire level is finished
+//    public void PlayHappyDance()
+//    {
+//        if (animator != null) animator.Play("HappyDance", -1, 0f);
+//        Debug.Log("HappyDance");
+//    }
+
+//    // Optionally call this to force return to Idle, 
+//    // though transitions with "Exit Time" in the Animator are better.
+//    public void PlayIdle()
+//    {
+//        //if (animator != null) animator.Play("Idle");
+//        if (animator != null) animator.Play("Ele_Idle");
+//        //Debug.Log("Idle");
+//        Debug.Log("Ele_Idle");
+//    }
+//}
+
+
+using UnityEngine;
+using System.Collections;
 
 public class MixAFix_DollController : MonoBehaviour
 {
     public static MixAFix_DollController Instance;
 
     private Animator animator;
+
+    [Header("Animation Names")]
+    public string idleAnimation = "Ele_Idle";
+    public string successAnimation = "Ele_Yes";
+    public string sadAnimation = "Ele_No";
+    public string happyDanceAnimation = "Ele_Yes";
 
     private void Awake()
     {
@@ -15,29 +73,41 @@ public class MixAFix_DollController : MonoBehaviour
     // Triggered when a correct ingredient is accepted
     public void PlaySuccess()
     {
-        if (animator != null) animator.Play("Success", -1, 0f);
-        Debug.Log("Success");
+        if (animator != null)
+            StartCoroutine(PlayThenIdle(successAnimation));
     }
 
     // Triggered when a wrong ingredient is dropped
     public void PlaySad()
     {
-        if (animator != null) animator.Play("Sad", -1, 0f);
-        Debug.Log("Sad");
+        if (animator != null)
+            StartCoroutine(PlayThenIdle(sadAnimation));
     }
 
     // Triggered when the entire level is finished
     public void PlayHappyDance()
     {
-        if (animator != null) animator.Play("HappyDance", -1, 0f);
-        Debug.Log("HappyDance");
+        if (animator != null)
+            //animator.Play(happyDanceAnimation, -1, 0f);
+            animator.Play(happyDanceAnimation);
     }
 
-    // Optionally call this to force return to Idle, 
-    // though transitions with "Exit Time" in the Animator are better.
     public void PlayIdle()
     {
-        if (animator != null) animator.Play("Idle");
-        Debug.Log("Idle");
+        if (animator != null)
+            animator.Play(idleAnimation);
+    }
+
+    private IEnumerator PlayThenIdle(string animName)
+    {
+        //animator.Play(animName, -1, 0f);
+        animator.Play(animName);
+
+        yield return null; // wait 1 frame so Animator updates
+
+        float length = animator.GetCurrentAnimatorStateInfo(0).length;
+        yield return new WaitForSeconds(length);
+
+        animator.Play(idleAnimation);
     }
 }
